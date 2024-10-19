95703a907ce69d2222a663df44a9ffc25edf0623
James Wu <jameswu@Jamess-MacBook-Air-2.local>
2024-10-19 15:41:32 -0500

    Signup flow integrated with api

--- api/src/server.js
+++ api/src/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors");
+
 const eventRoutes = require("./routes/eventRoutes");
 const userRoutes = require("./routes/userRoutes");
 require("./cron/jobs");
@@ -8,6 +10,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
+
 app.use("/events", eventRoutes);
 app.use("/user", userRoutes);
 
