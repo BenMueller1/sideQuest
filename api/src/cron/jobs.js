418001c898357f6996cbd192e6337de627f93d2e
James Wu <jameswu@Jamess-MacBook-Air-2.local>
2024-10-19 14:35:01 -0500

    initial setup for embarkments

---
+++ api/src/cron/jobs.js
@@ -0,0 +1,34 @@
+const cron = require("node-cron");
+const prisma = require("./../models/index");
+
+const bucketByEvent = (embarkations) => {
+  let eventSignups = {};
+
+  for (let embarkation in embarkations) {
+    let eventId = embarkation.eventId;
+
+    if (eventId in eventSignups) {
+      eventSignups[eventId].append(embarkation);
+    } else {
+      eventSignups[eventId] = [embarkation];
+    }
+  }
+
+  return eventSignups;
+};
+
+async function matchingService() {
+  try {
+    const embarkations = await prisma.embarkations.findMany();
+
+    const eventSignups = bucketByEvent(embarkations);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+cron.schedule("0 12 * * *", () => {
+  console.log("Matching embarkations into groups");
+});
+
+module.exports = cron;
