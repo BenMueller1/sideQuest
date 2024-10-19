fbe012fce923066bbc1c6ef12ee54a89157e62d7
Ben Mueller <benjm2002@gmail.com>
2024-10-19 15:58:07 -0500

    fix cors issue in server

--- api/src/server.js
+++ api/src/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const cors = require('cors');
 const eventRoutes = require("./routes/eventRoutes");
 require("./cron/jobs");
+require('dotenv').config();
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
+app.use(cors());
 app.use("/events", eventRoutes);
 
 app.listen(PORT, () => {
